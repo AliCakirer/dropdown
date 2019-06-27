9c39e9f77fb6b8f78a095816971ff2baaa571c40
mehmetcalikus <mehmetcalikus@gmail.com>
2019-06-27 17:10:10 +0300

    1-2 fonksiyon eklendi..

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
-
 import Box from './box'
+class Dropdown extends Component {
+constructor(props){
+    super(props)
+    this.state={
+    selectedCity:null  
+    }
+    this.selectCity = this.selectCity.bind(this)
+}
 
-export default class index extends Component {
+selectCity(event) {
+    this.setState=({
+        selectedCity:event.target.value
+    })
+}
+
+
     render() {
         return (
             <div>
                 <Box
                 handleClick = {this.onSelect}
                 data = {this.state.data}
+                onClick={this.selectCity}
                 />
             </div>
+
         )
     }
 }
+export default Dropdown
