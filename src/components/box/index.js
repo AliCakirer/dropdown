6a96312a91ba1e860cd3f0c4d4a443cb6f16ae84
mehmetcalikus <mehmetcalikus@gmail.com>
2019-06-27 11:51:55 +0300

    ilk kısım tamam..

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react'
 
 
 
+
 class Box extends Component {
+
     render() {
+        const dataList = this.props.data.map((element) =>
+
+           
+        <li key ={element.name} onClick={this.handleClick} value={element} id={element.name}>{element.name}</li>
+    
+        );
+
         return (
             <div>
-                
+            <ul>{dataList}</ul>
             </div>
         )
     }
