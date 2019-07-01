4ee20679e2b3157e56f414ef47f76bd90a98e9be
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2019-07-01 15:10:25 +0300

    input visual change

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -5,7 +5,7 @@ class Box extends Component {
 
     render() {
         const dataList = this.props.data.map((element) =>
-            <li className="list-group-item my-item " key={element.name} onClick={()=>this.props.handleClick(element)} value={element} id={element.name}>{element.name}</li>
+            <li className="list-group-item my-item" key={element.name} onClick={()=>this.props.handleClick(element)} value={element} id={element.name}>{element.name}</li>
         );
 
         return (
